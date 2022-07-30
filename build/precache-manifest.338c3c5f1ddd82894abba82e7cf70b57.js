cbcbda86b20bf133b94d498b1e19beaddc848005
Siri-Pulakanti <siripulakanti1@gmail.com>
2022-07-30 19:42:10 +0530

    deployed using firebase

---
+++ build/precache-manifest.338c3c5f1ddd82894abba82e7cf70b57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23bd52d74ba465d756aeb03d1d441522",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b53e588f42268306d4a",
+    "url": "/static/css/main.b345df2f.chunk.css"
+  },
+  {
+    "revision": "2679ca319e2094235dfd",
+    "url": "/static/js/2.da850c9a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.da850c9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b53e588f42268306d4a",
+    "url": "/static/js/main.a59ebba3.chunk.js"
+  },
+  {
+    "revision": "9eb3a9a40513b5c83cbc",
+    "url": "/static/js/runtime-main.bb6addc8.js"
+  }
+]);
